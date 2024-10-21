8b49a10309dfdcdc78a7ef165135331b0e85a062
Alrik Künne <alrik.kuenne@pwc.com>
2024-10-21 15:59:19 +0000

    changed back srv

--- srv/services.js
+++ srv/services.js
@@ -5,7 +5,7 @@ class ProcessorService extends cds.ApplicationService {
   init() {
     this.before("UPDATE", "Incidents", (req) => this.onUpdate(req));
     this.before("CREATE", "Incidents", (req) => this.changeUrgencyDueToSubject(req.data));
-    this.after("each", "Incidents", (incident) => { incident.title += ' changed' });
+    //this.after("each", "Incidents", (incident) => { incident.title += ' changed' });
 
     return super.init();
   }
