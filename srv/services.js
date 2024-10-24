bf5d961a02d9c9c99636740c765d8190ae36ffb7
Alrik Künne <alrik.kuenne@pwc.com>
2024-10-24 12:24:32 +0000

    added closed check

--- srv/services.js
+++ srv/services.js
@@ -3,6 +3,7 @@ const cds = require('@sap/cds')
 class ProcessorService extends cds.ApplicationService {
   /** Registering custom event handlers */
   init() {
+    this.before("UPDATE", "Incidents", (req) => this.onUpdate(req));
     this.before("CREATE", "Incidents", (req) => this.changeUrgencyDueToSubject(req.data));
     this.after("each", "Incidents", (incident) => { 
       incident.title += '🚩';
@@ -19,6 +20,12 @@ class ProcessorService extends cds.ApplicationService {
         }
       });
     }
+  }
+
+  async onUpdate (req) {
+    const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ID: req.data.ID})
+    if (status_code === 'C')
+      return req.reject(`Can't modify a closed incident`)
   }
 
 }
