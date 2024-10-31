b4e07b9162cca109bcec32ec8eee296536bf8bec
alrikx <35694442+alrikx@users.noreply.github.com>
2024-10-31 16:37:09 +0000

    Added some // cra[[]]()zy stuff
    Test

--- .github/workflows/scripts/determine_ctms_msg.sh
+++ .github/workflows/scripts/determine_ctms_msg.sh
@@ -7,14 +7,14 @@ echo "COMMIT_SHORT_SHA=$calculatedSha" >> $GITHUB_ENV
 
 #remove linebreaks
 STAGE1=$(echo $gh_msg | tr -d '\n')
-echo "stage1"
-echo $STAGE1
+#echo "stage1"
+#echo $STAGE1
 
 #remove whitespaces
 STAGE2=${STAGE1// /_}
-echo "stage2"
-echo $STAGE2
+#echo "stage2"
+#echo $STAGE2
 
 CONCAT_MSG_TMP=$(echo "$calculatedSha-$STAGE2")
-echo  $CONCAT_MSG_TMP
+#echo  $CONCAT_MSG_TMP
 echo "CONCAT_MSG=$(echo $CONCAT_MSG_TMP | cut -c 1-512)" >> $GITHUB_ENV
