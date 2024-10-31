0918eb3ae3dde758b1d3ea065a630ad73ae4c4c8
alrikx <35694442+alrikx@users.noreply.github.com>
2024-10-31 16:19:19 +0000

    Addded script for determination

---
+++ .github/workflows/scripts/determine_ctms_msg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+#calc short sha
+#calculatedSha=$(git rev-parse --short $gh_id)
+#echo "COMMIT_SHORT_SHA=$calculatedSha" >> $GITHUB_ENV
+
+
+calculatedSha='123345456'
+gh_msg="Test \n tets 2"
+
+echo $gh_msg
+
+#remove linebreaks
+STAGE1=$(echo $gh_msg | tr -d '\n')
+echo "stage1"
+echo $STAGE1
+
+
+#remove whitespaces
+STAGE2=${STAGE1// /_}
+echo "stage2"
+echo $STAGE2
+
+
+CONCAT_MSG_TMP=$(echo "$calculatedSha-$STAGE2")
+echo  $CONCAT_MSG_TMP
+echo "CONCAT_MSG=$(echo $CONCAT_MSG_TMP | cut -c 1-512)" >> $GITHUB_ENV
