fb0fbde7ba389a346073e5cdd28a621a0e408dcf
Alrik Künne <alrik.kuenne@pwc.com>
2024-11-07 14:34:01 +0000

    added autumn

--- srv/services.js
+++ srv/services.js
@@ -20,6 +20,9 @@ class ProcessorService extends cds.ApplicationService {
       if (lowerTitle.includes('account')) {  
         incident.title += `🎉🎉🎉`;
       }
+      if (lowerTitle.includes('autumn')) {  
+        incident.title += `🍂🍂🍂`;
+      }
     });
     return super.init();
   }
